096f07ce25cd378fe0b0f5ef4480ab0ce9fbd157
sangpyokim <rlatkdvy12@naver.com>
2022-12-01 20:02:08 +0900

    [layout] 미디어 쿼리 사용

--- components/organisms/Navigator.tsx
+++ components/organisms/Navigator.tsx
@@ -1,47 +1,78 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 import style from './Navigator.module.css'
-import { DashboardFilled, DashboardOutlined, HeartOutlined, HeartFilled, CommentOutlined } from '@ant-design/icons'
+import {
+  DashboardFilled,
+  DashboardOutlined,
+  HeartOutlined,
+  HeartFilled,
+  CommentOutlined,
+} from '@ant-design/icons'
 import Link from 'next/link'
 
 const Navigator = () => {
   const router = useRouter()
 
   return (
-    <div className={style.container} >
-      { 
-        router.pathname === '/' ?
-        <Link href={'/'} >
-          <DashboardFilled alt='timer-fill' style={styles.icon} />
+    <div className={style.container}>
+      <div className={style.linkWrapper}>
+        <Link href={'/'}>
+          <div className={style.iconWrapper}>
+            {router.pathname === '/' ? (
+              <DashboardFilled
+                alt="timer-fill"
+                style={styles.icon}
+              />
+            ) : (
+              <DashboardOutlined
+                alt="timer-outline"
+                style={styles.icon}
+              />
+            )}
+            <p className={style.iconName}>타이머</p>
+          </div>
         </Link>
-        :
-        <Link href={'/'} >
-          <DashboardOutlined alt='timer-outline' style={styles.icon} />
-        </Link>
-      }
-      { 
-        router.pathname === '/diet' ?
-        <Link href={'/diet'} >
-          <HeartFilled alt='heart-fill' style={styles.icon} />
-        </Link>
-        :
-        <Link href={'/diet'} >
-          <HeartOutlined alt='heart-outline' style={styles.icon} />
-        </Link>
-      }
+      </div>
 
-        <Link href={'/community'} >
-          <CommentOutlined alt='comment-outline' style={styles.icon} />
+      <div className={style.linkWrapper}>
+        <Link href={'/diet'}>
+          <div className={style.iconWrapper}>
+            {router.pathname === '/diet' ? (
+              <HeartFilled
+                alt="heart-fill"
+                style={styles.icon}
+              />
+            ) : (
+              <HeartOutlined
+                alt="heart-outline"
+                style={styles.icon}
+              />
+            )}
+            <p className={style.iconName}>식단</p>
+          </div>
         </Link>
+      </div>
+
+      <div className={style.linkWrapper}>
+        <Link href={'/community'}>
+          <div className={style.iconWrapper}>
+            <CommentOutlined
+              alt="comment-outline"
+              style={styles.icon}
+            />
+            <p className={style.iconName}>커뮤니티</p>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
 
 const styles = {
   icon: {
-    fontSize: '26px'
+    fontSize: '24px',
+    marginRight: '6px',
   },
 }
-
 
 export default Navigator
